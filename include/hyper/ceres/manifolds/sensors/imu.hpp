86c9ae29c4e72c0a22fa4ed3e3667309e46db22f
alkynee <alkynee@proton.me>
2023-03-13 09:04:34 +0000

    Fix small bugs

--- include/hyper/ceres/manifolds/sensors/imu.hpp
+++ include/hyper/ceres/manifolds/sensors/imu.hpp
@@ -20,3 +20,29 @@ class Manifold<sensors::IMU> final : public Manifold<sensors::Sensor> {
   static constexpr auto kGyroscopeBiasSubmanifoldIndex = IMU::kNumVariables;
   static constexpr auto kAccelerometerBiasSubmanifoldIndex = kGyroscopeBiasSubmanifoldIndex + 1;
   static constexpr auto kNumSubmanifolds = kAccelerometerBiasSubmanifoldIndex + 1;
+
+  /// Default constructor.
+  /// \param imu IMU to parametrize.
+  /// \param constant Constancy flag.
+  explicit Manifold(IMU* imu, bool constant = true);
+
+  /// Sensor accessor.
+  /// \return Sensor.
+  [[nodiscard]] auto sensor() const -> const IMU* final;
+
+  /// Gyroscope intrinsics submanifold accessor.
+  [[nodiscard]] auto gyroscopeIntrinsicsSubmanifold() const -> Submanifold*;
+
+  /// Sets the gyroscope intrinsics submanifold.
+  /// \param submanifold Input submanifold.
+  auto setGyroscopeIntrinsicsSubmanifold(std::unique_ptr<Submanifold>&& submanifold) -> void;
+
+  /// Sets the sensor's gyroscope intrinsics constant or variable.
+  /// \param constant Constancy flag.
+  auto setGyroscopeIntrinsicsConstant(bool constant) -> void;
+
+  /// Gyroscope sensitivity submanifold accessor.
+  [[nodiscard]] auto gyroscopeSensitivitySubmanifold() const -> Submanifold*;
+
+  /// Sets the gyroscope sensitivity submanifold.
+  /// \param su
