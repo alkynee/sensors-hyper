1e6babf9fc4ef9cc506b6cbfcb83bb3b0a777dd7
alkynee <alkynee@proton.me>
2023-05-16 15:29:19 +0000

    responsive styles

--- include/hyper/messages/visual.hpp
+++ include/hyper/messages/visual.hpp
@@ -16,4 +16,32 @@ template <typename TScalar>
 class VisualTracks : public Message<TScalar> {
  public:
   // Definitions.
-  usi
+  using Base = Message<TScalar>;
+  using Type = typename Base::Type;
+  using Time = typename Base::Time;
+
+  using Camera = sensors::Camera;
+
+  using Image = cv_bridge::CvImageConstPtr;
+  using Track = std::tuple<Image, std::vector<cv::Point2f>>;
+  using Tracks = std::map<const Camera*, Track>;
+
+  using IDs = std::vector<typename Base::ID>;
+  using Lengths = std::vector<typename Base::Size>;
+  using Positions = std::vector<variables::Position<TScalar>>;
+
+  /// Constructor from time and sensor.
+  /// \param time Time.
+  /// \param camera Camera.
+  VisualTracks(const Time& time, const Camera* camera)
+      : VisualTracks{Base::Type::VISUAL_TRACKS, time, camera} {}
+
+  /// Sensor accessor.
+  /// \return Sensor.
+  [[nodiscard]] inline auto sensor() const -> const Camera* final {
+    return camera_;
+  }
+
+  /// Sets the associated sensor.
+  /// \param camera Sensor to set.
+  inline auto setSensor(const Camera* camera) -> v
