3309c586f88fc8e73e69cfb06c0d9ae5070ce059
alkynee <alkynee@proton.me>
2023-05-16 22:17:47 +0000

    compiler

--- include/hyper/sensors/imu.hpp
+++ include/hyper/sensors/imu.hpp
@@ -17,4 +17,21 @@ class IMU final : public Sensor {
   static constexpr auto kAccelerometerBiasPartitionIndex = kGyroscopeBiasPartitionIndex + 1;
   static constexpr auto kNumPartitions = kAccelerometerBiasPartitionIndex + 1;
 
-  static conste
+  static constexpr auto kGyroscopeIntrinsicsIndex = Sensor::kNumVariables;
+  static constexpr auto kGyroscopeSensitivityIndex = kGyroscopeIntrinsicsIndex + 1;
+  static constexpr auto kAccelerometerIntrinsicsIndex = kGyroscopeSensitivityIndex + 1;
+  static constexpr auto kAccelerometerOffsetIndex = kAccelerometerIntrinsicsIndex + 1;
+  static constexpr auto kNumVariables = kAccelerometerOffsetIndex + 1;
+
+  // Definitions.
+  using GyroscopeNoiseDensity = Scalar;
+  using GyroscopeIntrinsics = variables::OrthonormalityAlignment<Scalar, 3>;
+  using GyroscopeSensitivity = variables::Sensitivity<Scalar, 3>;
+  using GyroscopeBias = state::ContinuousState<variables::Rn<Scalar, 3>>;
+
+  using AccelerometerNoiseDensity = Scalar;
+  using AccelerometerIntrinsics = variables::OrthonormalityAlignment<Scalar, 3>;
+  using AccelerometerOffset = variables::Rn<Scalar, 9>;
+  using AccelerometerBias = state::ContinuousState<variables::Rn<Scalar, 3>>;
+
+  using BiasInterpolat
