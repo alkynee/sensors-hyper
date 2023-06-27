6e161856c8dfe8e249d5a605fbb34f5ff32d5b19
alkynee <alkynee@proton.me>
2023-06-27 17:01:08 +0000

    Remove commented code

--- include/hyper/sensors/sensor.hpp
+++ include/hyper/sensors/sensor.hpp
@@ -18,4 +18,36 @@ class Sensor {
   static constexpr auto kNumPartitions = kSensorPartitionIndex + 1;
 
   static constexpr auto kOffsetIndex = 0;
-  static constexpr auto kTransformationIn
+  static constexpr auto kTransformationIndex = kOffsetIndex + 1;
+  static constexpr auto kNumVariables = kTransformationIndex + 1;
+
+  static constexpr auto kDefaultJacobianType = JacobianType::TANGENT_TO_MANIFOLD;
+
+  // Definitions.
+  using Node = YAML::Node;
+  using Emitter = YAML::Emitter;
+
+  using Time = double;
+  using Scalar = double;
+  using ParameterBlocks = std::vector<Scalar*>;
+  using ParameterBlockSizes = std::vector<int>;
+
+  using Rate = Scalar;
+  using Offset = variables::R1<Scalar>;
+  using Transformation = variables::SE3<Scalar>;
+
+  /// Constructor from Jacobian type.
+  /// \param jacobian_type Jacobian type.
+  explicit Sensor(JacobianType jacobian_type = kDefaultJacobianType);
+
+  /// Constructor from YAML node.
+  /// \param node YAML node.
+  explicit Sensor(const Node& node);
+
+  /// Default destructor.
+  virtual ~Sensor() = default;
+
+  /// Downcast this instance.
+  /// \tparam TDerived_ Target type.
+  /// \return Reference to cast instance.
+  template <typename
