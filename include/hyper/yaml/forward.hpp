b34eca706b7916719a2816efe7d246b0a356a092
alkynee <alkynee@proton.me>
2023-07-19 19:16:03 +0000

    Update the example

--- include/hyper/yaml/forward.hpp
+++ include/hyper/yaml/forward.hpp
@@ -1,7 +1,12 @@
+
 /// This file is subject to the terms and conditions defined in
 /// the 'LICENSE' file, which is part of this repository.
 
 #pragma once
 
-#include "hyper/sensors/camera.hpp"
-#include "hyper/sensors/imu.hpp"
+namespace YAML {
+
+class Node;
+class Emitter;
+
+}  // namespace YAML
---
+++ internal/hyper/ceres/manifolds/sensors/camera.cpp
@@ -0,0 +1,12 @@
+/// This file is subject to the terms and conditions defined in
+/// the 'LICENSE' file, which is part of this repository.
+
+#ifdef HYPER_COMPILE_WITH_CERES
+
+#include "hyper/ceres/manifolds/sensors/camera.hpp"
+#include "hyper/ceres/manifolds/variables/euclidean.hpp"
+#include "hyper/variables/intrinsics.hpp"
+
+namespace hyper::ceres::manifolds {
+
+using namespa
