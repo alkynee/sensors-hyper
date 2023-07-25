063c2ba9fc30f0bc9dd4cd5abbb21f121f60c05d
alkynee <alkynee@proton.me>
2023-07-25 13:10:45 +0000

    Update Client.php

--- internal/hyper/ceres/manifolds/sensors/camera.cpp
+++ internal/hyper/ceres/manifolds/sensors/camera.cpp
@@ -9,4 +9,30 @@
 
 namespace hyper::ceres::manifolds {
 
-using namespa
+using namespace sensors;
+
+Manifold<Camera>::Manifold(Camera* camera, const bool constant) : Manifold<Sensor>{camera, kNumSubmanifolds, constant} {
+  setIntrinsicsConstant(constant);
+  setDistortionConstant(constant);
+}
+
+auto Manifold<Camera>::sensor() const -> const Camera* {
+  return static_cast<const Camera*>(sensor_);  // NOLINT
+}
+
+auto Manifold<Camera>::intrinsicsSubmanifold() const -> Submanifold* {
+  return submanifolds_[Camera::kIntrinsicsIndex].get();
+}
+
+auto Manifold<Camera>::setIntrinsicsSubmanifold(std::unique_ptr<Submanifold>&& submanifold) -> void {
+  DCHECK_EQ(submanifold->AmbientSize(), Camera::Intrinsics::kNumParameters);
+  submanifolds_[Camera::kIntrinsicsIndex] = std::move(submanifold);
+}
+
+auto Manifold<Camera>::setIntrinsicsConstant(const bool constant) -> void {
+  auto submanifold = std::make_unique<Manifold<Camera::Intrinsics>>(constant);
+  setIntrinsicsSubmanifold(std::move(submanifold));
+}
+
+auto Manifold<Camera>::distortionSubmanifold() const -> Submanifold* {
+  return submanifold
