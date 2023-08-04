054a07b3a6e1084b6f4a65f3376188803d06333f
alkynee <alkynee@proton.me>
2023-08-04 09:05:50 +0000

    Run update-remote.sh on elasticsearch

--- internal/hyper/sensors/sensor.cpp
+++ internal/hyper/sensors/sensor.cpp
@@ -57,4 +57,34 @@ auto Sensor::parameterBlockSizes() const -> const ParameterBlockSizes& {
   return parameter_block_sizes_;
 }
 
-auto Sensor::offset() const -> co
+auto Sensor::offset() const -> const Offset& {
+  return static_cast<const Offset&>(*variables_[kOffsetIndex]);  // NOLINT
+}
+
+auto Sensor::offset() -> Offset& {
+  return const_cast<Offset&>(std::as_const(*this).offset());
+}
+
+auto Sensor::transformation() const -> const Transformation& {
+  return static_cast<const Transformation&>(*variables_[kTransformationIndex]);  // NOLINT
+}
+
+auto Sensor::transformation() -> Transformation& {
+  return const_cast<Transformation&>(std::as_const(*this).transformation());
+}
+
+auto Sensor::partitions(const Time& /* time */) const -> Partitions<Scalar*> {
+  Partitions<Scalar*> partitions{kNumPartitions};
+  partitions[kSensorPartitionIndex] = assembleVariablesPartition();
+  return partitions;
+}
+
+auto operator>>(const YAML::Node& node, Sensor& sensor) -> const YAML::Node& {
+  CHECK(node);
+  sensor.read(node);
+  return node;
+}
+
+auto operator<<(YAML::Emitter& emitter, const Sensor& sensor) -> YAML::Emitter& {
+  emitter << YAML::BeginMap;
+  sensor.write(emit
