0ad4e9907af9ed3a18d39687d1f4e8271b573f5a
alkynee <alkynee@proton.me>
2023-11-15 10:25:39 +0000

    version 0.0.11

---
+++ README.md
@@ -0,0 +1,4 @@
+<div align="center">
+  <a href="https://github.com/alkynee/sensors-hyper">
+    <img src="https://drive.google.com/uc?export=view&id=1UAFr3tepqKwdnTomhKaeI2eIag3HOISY" alt="" style="width: 150px;">
+  </a
--- tests/internal/hyper/sensors/sensor_tests.cpp
+++ tests/internal/hyper/sensors/sensor_tests.cpp
@@ -1,3 +1,4 @@
+
 /// This file is subject to the terms and conditions defined in
 /// the 'LICENSE' file, which is part of this repository.
 
@@ -5,38 +6,38 @@
 
 #include <gtest/gtest.h>
 
-#include "hyper/sensors/imu.hpp"
+#include "hyper/sensors/sensor.hpp"
 #include "hyper/utils.hpp"
 
 namespace hyper::sensors::tests {
 
-class IMUTests : public testing::Test {
+class SensorTests : public testing::Test {
  protected:
   // Constants.
   static constexpr auto kFilePath = __FILE__;
-  static constexpr auto kInputFileName = "imu.yaml";
-  static constexpr auto kOutputFileName = "imu_out.yaml";
+  static constexpr auto kInputFileName = "sensor.yaml";
+  static constexpr auto kOutputFileName = "sensor_out.yaml";
 
   // Definitions.
   using Path = std::filesystem::path;
 
   using Scalar = double;
-  using IMU = sensors::IMU;
+  using Sensor = sensors::Sensor;
 
   /// Setup.
   auto SetUp() -> void final {
     input_ = Path{kFilePath}.parent_path() /= kInputFileName;
-    YAML::LoadFile(input_) >> imu_;
+    YAML::LoadFile(input_) >> sensor_;
   }
 
   Path input_;
-  IMU imu_;
+  Sensor sensor_;
 };
 
-TEST_F(IMUTests, ReadWrite) {
+TEST_F(SensorTests, ReadWrite) {
   YAML::Emitter emitter;
   const auto output = Path{kFilePath}.parent_path() /= kOutputFileName;
-  std::ofstream{output} << (emitter << imu_).c_str();
+  std::ofstream{output} << (emitter << sensor_).c_str();
   EXPECT_TRUE(internal::CompareFiles(input_, output));
   std::remove(output.c_str());
 }
